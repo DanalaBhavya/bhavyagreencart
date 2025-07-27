917f230878247cb7773ebc1bce935f0821b67023
DanalaBhavya <139364651+DanalaBhavya@users.noreply.github.com>
2025-07-27 19:33:58 +0530

    Update db.js

--- server/configs/db.js
+++ server/configs/db.js
@@ -4,7 +4,7 @@ const connectDB = async()=>{
     try{
         mongoose.connection.on('connected',()=>console.log
         ("Database Connected"));
-        await mongoose.connect(`${process.env.MONGODB_URI}/test`)
+        await mongoose.connect(`${process.env.MONGODB_URI}/bhavyaCart1`)
     }catch(error){
         console.error(error.message);
     }

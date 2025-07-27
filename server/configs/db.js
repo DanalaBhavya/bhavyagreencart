c92a66bc1a9f0c09ed9d6388c35938b5c28088cc
DanalaBhavya <139364651+DanalaBhavya@users.noreply.github.com>
2025-07-27 19:04:05 +0530

    Update db.js

--- server/configs/db.js
+++ server/configs/db.js
@@ -4,7 +4,7 @@ const connectDB = async()=>{
     try{
         mongoose.connection.on('connected',()=>console.log
         ("Database Connected"));
-        await mongoose.connect(`${process.env.MONGODB_URI}/Jagacart`)
+        await mongoose.connect(`${process.env.MONGODB_URI}/test`)
     }catch(error){
         console.error(error.message);
     }
